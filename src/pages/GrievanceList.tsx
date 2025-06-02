bdf0f687261967409b47202a247adf14eac47d29
gourav094 <gouravgarg012@gmail.com>
2025-06-02 10:26:10 +0530

    reduce api calls in failing case

--- src/pages/GrievanceList.tsx
+++ src/pages/GrievanceList.tsx
@@ -24,6 +24,7 @@ const GrievanceList = () => {
         ? grievanceApi.getAllGrievances() // admin: fetch all
         : grievanceApi.getAllGrievances(user.name), // user: only their grievances
     enabled: !!user,
+    retry: 0,
   });
 
   const filteredGrievances = useMemo(() => {
